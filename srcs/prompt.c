115da1707b03cfa941c5680e1b34aff65eab2348
Marine <marine@MacBook-Air-de-Marine-2.local>
2023-06-29 22:49:21 +0200

    added management of ctrl d and readline null to the prompt

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -6,7 +6,7 @@
 /*   By: marine <marine@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 16:29:29 by marine            #+#    #+#             */
-/*   Updated: 2023/06/27 18:04:23 by marine           ###   ########.fr       */
+/*   Updated: 2023/06/29 18:45:11 by marine           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,17 @@ void	prompt(void)
 	{
 		input = readline("aristoshell$ ");
 		add_history(input);
-		if (strcmp(input, "exit") == 0)
+		if (!input || ft_strncmp(input, "exit", ft_strlen(input) + 1) == 0)
 		{
+			printf("exit\n");
 			free(input);
 			clear_history();
 			return ;
 		}
-		if (input != 0)
+		if (input && input[0] != 0)
+		{
 			printf("t'as dit \"%s\"\n", input);
+			free(input);
+		}
 	}
 }
