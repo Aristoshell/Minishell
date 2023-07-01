d73fe118ff5b9f48ecadc81f1ab3a2e02f8e9397
Marine <marine@MacBook-Air-de-Marine-2.local>
2023-07-01 16:49:01 +0200

    added argv argc in main

--- srcs/main.c
+++ srcs/main.c
@@ -6,14 +6,19 @@
 /*   By: marine <marine@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 16:29:29 by marine            #+#    #+#             */
-/*   Updated: 2023/06/27 18:05:07 by marine           ###   ########.fr       */
+/*   Updated: 2023/07/01 16:23:14 by marine           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	prompt();
-	return (0);
+	(void) argv;
+	if (argc == 1)
+	{
+		prompt();
+		return (0);
+	}
+	return (perror("Error : Please launch minishell with no additional argument"), -1);
 }
