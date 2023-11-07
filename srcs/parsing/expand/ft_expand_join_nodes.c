bcba93bf548a35f0c3f983cffda0f491aef1d085
rinmadv <marine.mardavid@gmail.com>
2023-11-07 20:56:28 +0100

    fixed expand but still more to do

--- srcs/parsing/expand/ft_expand_join_nodes.c
+++ srcs/parsing/expand/ft_expand_join_nodes.c
@@ -13,34 +13,26 @@ int	ft_join_nodes(t_list *list, t_data *data)
 	while (list)
 	{
 		current_token = (t_token *)list->content;
-		while (list && !current_token->join_with_next) //passer les non join
-		{
+		if (!current_token->join_with_next) //passer les non join
 			list = list->next;
-			if (list)
-				current_token = (t_token *)list->content;
-		}
-		while (list && current_token->join_with_next) 
+		else
 		{
 			next_node = list->next;
 			if (next_node)
 				next_token = (t_token *)next_node->content;
-			if (next_node)
-			{
-				join = ft_strjoin(current_token->string, next_token->string);
-				if (!join)
-					return (MEMORY_ERR_NB);
-				free(current_token->string);
-				current_token->string = join;
-				current_token->join_with_next = next_token->join_with_next;
-				t_list *save;
-				save = list->next->next;
-				ft_lstdelone(list->next, (void *)ft_clean_token);
-				list->next = save;
-			}
-			list = list->next;
+			join = ft_strjoin(current_token->string, next_token->string);
+			if (!join)
+				return (MEMORY_ERR_NB);
+			free(current_token->string);
+			current_token->string = join;
+			current_token->join_with_next = next_token->join_with_next;
+			t_list *save;
+			save = list->next->next;
+			ft_lstdelone(list->next, (void *)ft_clean_token);
+			list->next = save;
 		}
 	}
-	// printf("JOIN NODES\n");
-	// ft_display_lexer(*data);
+	//printf("JOIN NODES\n");
+	//ft_display_lexer(*data);
 	return (FUNCTION_SUCCESS);
 }
